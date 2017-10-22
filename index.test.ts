cfd628f724e8ed19ee59b7beaedd4a2fd9584f5d
Git Automerge <jcguarinpenaranda@gmail.com>
2017-10-22 14:44:14 -0500

    first commit

---
+++ index.test.ts
@@ -0,0 +1,59 @@
+import * as Parse from 'parse/node';
+import ParseQueryGen from './index';
+
+test('should proccess ok the params', () => {
+  ParseQueryGen.setParse(Parse);
+  const params = {
+    className: 'MyClass',
+    equalTo: {
+      key: 'value',
+      key2: 'value2',
+    },
+    greaterThan: {
+      key3: 'value',
+      key4: 'value2',
+    },
+    lessThan: {
+      key5: 'value',
+      key6: 'value2',
+    },
+    notEqualTo: {
+      key7: 'abc',
+    },
+    ascending: ['createdAt'],
+    descending: ['updatedAt'],
+    containedIn: {
+      w: ['abc', '123'],
+    },
+    matches: {
+      string: 'value',
+    },
+    select: ['abc', 'abc2'],
+    containsAll: {
+      w2: ['abc', '123'],
+    },
+    include: ['brand'],
+  };
+
+  const q = ParseQueryGen.gen(params);
+
+  const json = q.toJSON();
+
+  expect(q instanceof Parse.Query);
+
+  expect(q.className).toEqual(params.className);
+  expect(json.where.key).toEqual(params.equalTo.key);
+  expect(json.where.key2).toEqual(params.equalTo.key2);
+  expect(json.where.key3).toEqual({ $gt: params.greaterThan.key3 });
+  expect(json.where.key4).toEqual({ $gt: params.greaterThan.key4 });
+  expect(json.where.key5).toEqual({ $lt: params.lessThan.key5 });
+  expect(json.where.key6).toEqual({ $lt: params.lessThan.key6 });
+  expect(json.where.key7).toEqual({ $ne: params.notEqualTo.key7 });
+  expect(json.where.w).toEqual({ $in: params.containedIn.w });
+  expect(json.where.w2).toEqual({ $all: params.containsAll.w2 });
+  expect(json.where.string).toEqual({ $regex: params.matches.string });
+  expect(json.include).toEqual('brand');
+  expect(json.keys).toEqual(params.select.join(','));
+  expect(json.order.indexOf('createdAt') !== -1).toBe(true);
+  expect(json.order.indexOf('updatedAt') !== -1).toBe(true);
+});
---
+++ index.ts
@@ -0,0 +1,146 @@
+/* @tscheck false */
+/* tslint:disable variable-name no-param-reassign */
+import * as assert from 'assert';
+import * as is from 'is';
+
+let parse;
+
+export interface IParams {
+  className?: string; 
+  equalTo?:{[key:string]: any}; 
+  containedIn?:{[key:string]: any};
+  notEqualTo?:{[key:string]: any}; 
+  lessThan?:{[key:string]: any};
+  greaterThan?:{[key:string]: any}; 
+  containsAll?: {[key:string]: any};  
+  include?: string[];
+  descending?: string[]; 
+  ascending?: string[]; 
+  query ? : any;
+  matches ? : {[key:string]: any;}; 
+  select ? : string[];
+}
+
+export default class ParseQueryGen {
+  /**
+   * 
+   * @param {Parse} _Parse 
+   */
+  static setParse(_Parse) {
+    parse = _Parse;
+  }
+
+  /**
+   * @param params
+   * @return {Parse.Query}
+   */
+  static gen(parameters: IParams = {}) {
+    // default
+    const params = Object.assign({}, parameters);
+    params.equalTo = params.equalTo || {};
+    params.notEqualTo = params.notEqualTo || {};
+    params.containedIn = params.containedIn || {};
+    params.containsAll = params.containsAll || {};
+    params.include = params.include || [];
+    params.ascending = params.ascending || [];
+    params.descending = params.descending || [];
+    params.greaterThan = params.greaterThan || {};
+    params.lessThan = params.lessThan || {};
+    params.matches = params.matches || {};
+    params.select = params.select || [];
+
+    // validation
+    assert.ok(is.object(params));
+    assert.ok(is.object(params.equalTo), JSON.stringify(params.equalTo));
+    assert.ok(is.object(params.matches), JSON.stringify(params.matches));
+    assert.ok(is.object(params.notEqualTo), JSON.stringify(params.notEqualTo));
+    assert.ok(is.object(params.containedIn), JSON.stringify(params.containedIn));
+    assert.ok(is.object(params.containsAll), JSON.stringify(params.containedIn));
+    assert.ok(is.array(params.include));
+    assert.ok(is.array(params.ascending));
+    assert.ok(is.array(params.descending));
+    assert.ok(is.array(params.select));
+    assert.ok(is.object(params.greaterThan));
+    assert.ok(is.object(params.lessThan));
+
+    const q = params.query || new parse.Query(params.className);
+
+    if (params.equalTo) {
+      Object.keys(params.equalTo).forEach((key) => {
+        if (params.equalTo[key]) {
+          q.equalTo(key, params.equalTo[key]);
+        }
+      });
+    }
+
+    if (params.notEqualTo) {
+      Object.keys(params.notEqualTo).forEach((key) => {
+        if (params.notEqualTo[key]) {
+          q.notEqualTo(key, params.notEqualTo[key]);
+        }
+      });
+    }
+
+    if (params.containedIn) {
+      Object.keys(params.containedIn).forEach((key) => {
+        if (params.containedIn[key]) {
+          q.containedIn(key, params.containedIn[key]);
+        }
+      });
+    }
+
+    if (params.containsAll) {
+      Object.keys(params.containsAll).forEach((key) => {
+        if (params.containsAll[key]) {
+          q.containsAll(key, params.containsAll[key]);
+        }
+      });
+    }
+
+    if (params.greaterThan) {
+      Object.keys(params.greaterThan).forEach((key) => {
+        if (params.greaterThan[key]) {
+          q.greaterThan(key, params.greaterThan[key]);
+        }
+      });
+    }
+
+    if (params.lessThan) {
+      Object.keys(params.lessThan).forEach((key) => {
+        if (params.lessThan[key]) {
+          q.lessThan(key, params.lessThan[key]);
+        }
+      });
+    }
+
+    if (params.matches) {
+      Object.keys(params.matches).forEach((key) => {
+        if (params.matches[key]) {
+          q.matches(key, params.matches[key]);
+        }
+      });
+    }
+
+    if (params.select.length) {
+      q.select(params.select);
+    }
+
+    if (params.include.length) {
+      q.include(params.include);
+    }
+
+    if (params.ascending) {
+      params.ascending.forEach((p) => {
+        q.addAscending(p);
+      });
+    }
+
+    if (params.descending) {
+      params.descending.forEach((p) => {
+        q.addDescending(p);
+      });
+    }
+
+    return q;
+  }
+}
